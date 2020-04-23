aa61eef9b9978d3c093dca6af80e96157ab6bb18
Farzad FARID <farzad.farid@kapten.com>
2020-04-23 19:11:27 +0200

    Write state machine

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,45 @@ enum State {
 
 fn machine_cycle(state: &State, c: char) -> (Option<char>, State) {
     match *state {
-        State::Normal => (),
-        State::Comment => (),
-        State::Upper => (),
-        State::Lower => (),
-    };
-
-    (Some('a'), State::Normal)
+        State::Normal => {
+            match c {
+                '#' => (None, State::Comment),
+                '^' => (None, State::Upper),
+                '_' => (None, State::Lower),
+                d => (Some(d), State::Normal),
+            }
+        },
+        State::Comment => {
+            match c {
+                '#' => (None, State::Normal),
+                _ => (None, State::Comment),
+            }
+        },
+        State::Upper => {
+            match c {
+                '^' => (None, State::Normal),
+                _ => (Some(c.to_ascii_uppercase()), State::Upper),
+            }
+        },
+        State::Lower => {
+            match c {
+                '_' => (None, State::Normal),
+                _ => (Some(c.to_ascii_lowercase()), State::Lower),
+            }
+        },
+    }
 }
 
 fn main() {
+    let mut state = State::Normal;
+    let input = "The quick brown fox # blah # jumps ^over^ the _LaZy_ dog.";
 
+    for c in input.chars() {
+        let s = machine_cycle(&state, c);
+        match s.0 {
+            Some(c) => print!("{}", c),
+            None => (),
+        }
+        state = s.1;
+    }
 }
