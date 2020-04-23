63f3475c92ec000bc691fd53a50ca60f39ce7d38
Farzad FARID <farzad.farid@kapten.com>
2020-04-23 18:49:21 +0200

    Add skeleton state machine and Run configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+enum State {
+    Normal,
+    Comment,
+    Upper,
+    Lower,
+}
+
+fn machine_cycle(state: &State, c: char) -> (Option<char>, State) {
+    match *state {
+        State::Normal => (),
+        State::Comment => (),
+        State::Upper => (),
+        State::Lower => (),
+    };
+
+    (Some('a'), State::Normal)
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
